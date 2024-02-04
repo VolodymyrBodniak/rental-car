6fd039540293097e5c9e5753c6fa3b8ead53b900
VolodymyrBodniak <bodnjak@gmail.com>
2024-02-04 18:43:38 +0100

    Root correction

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { persistor, store } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
-import App from './components/App/App';
+import App from './components/App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
